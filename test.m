41ff10e79bd5c3942d54977419af38b2fe3cd6dc
whulearner <whurser@gmail.com>
2018-03-17 11:22:26 +0800

    v0.1 用NVDI进行试算

--- test.m
+++ test.m
@@ -1,8 +1,11 @@
-A=[1 2 3]';
-B=[3 2 1];
-C=A*B;
-D=[1 -1 2;0 1 0;-2 0 1];
-E=C*D
-F=[1 5 7;4 6 8;1 4 9];
-G=E*F;
-H=E*F*G;%the third modify
+%不透水面识别试算
+I=imread('D:/test.jpg');
+BLUE=I(:,:,1);
+GREEN=I(:,:,2);
+RED=I(:,:,3);
+%先假装有红外波段
+NIR=GREEN; %红外波段反射值
+NVDI=double(NIR-RED)./double(NIR+RED); %NDVI 归一化差值植被指数
+imshow(NVDI);
+Binary=imbinarize(NVDI);
+imshow(Binary);
